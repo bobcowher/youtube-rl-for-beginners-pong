9e01d14b0d63e06807f56ae9cea2d34760359f16
Robert Cowher <bobcowher@gmail.com>
2025-02-20 17:54:08 -0600

    Training but not learning

---
+++ agent.py
@@ -0,0 +1,144 @@
+from buffer import ReplayBuffer
+from model import Model, soft_update, hard_update
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision.transforms as T
+import datetime
+import time
+from torch.utils.tensorboard import SummaryWriter
+import random
+import os
+
+class Agent():
+
+    def __init__(self, env, dropout, hidden_layer, learning_rate, step_repeat, gamma) -> None:
+
+        self.env = env
+
+        self.step_repeat = step_repeat
+
+        self.gamma = gamma
+
+        obs, info = self.env.reset()
+
+        obs = self.process_observation(obs)
+
+        self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+        self.memory = ReplayBuffer(max_size=500000, input_shape=obs.shape, n_actions=env.action_space.n, device=self.device)
+
+        self.model = Model(action_dim=env.action_space.n, hidden_dim=hidden_layer, dropout=dropout, observation_shape=obs.shape).to(self.device)
+
+        self.target_model = Model(action_dim=env.action_space.n, hidden_dim=hidden_layer, dropout=dropout, observation_shape=obs.shape).to(self.device)
+
+        # Initialize target networks with model parameters
+        self.target_model.load_state_dict(self.model.state_dict())
+
+        self.optimizer_1 = optim.Adam(self.model.parameters(), lr=learning_rate)
+
+        self.learning_rate = learning_rate
+
+        print(f"Initialized agents on device: {self.device}")
+
+
+    def process_observation(self, obs):
+        obs = torch.tensor(obs, dtype=torch.float32).permute(2,0,1)  
+        return obs
+
+
+    def train(self, episodes, max_episode_steps, summary_writer_suffix, batch_size, epsilon, epsilon_decay, min_epsilon):
+        summary_writer_name = f'runs/{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}_{summary_writer_suffix}'
+        writer = SummaryWriter(summary_writer_name)
+
+        if not os.path.exists('models'):
+            os.makedirs('models')
+
+        total_steps = 0
+
+        for episode in range(episodes):
+
+            done = False
+            episode_reward = 0
+            obs, info = self.env.reset()
+            obs = self.process_observation(obs)
+
+            episode_steps = 0
+
+            episode_start_time = time.time()
+
+            while not done and episode_steps < max_episode_steps:
+
+                if random.random() < epsilon:
+                    action = self.env.action_space.sample()
+                else:
+                    q_values = self.model.forward(obs.unsqueeze(0).to(self.device))[0]
+                    action = torch.argmax(q_values, dim=-1).item()
+
+                reward = 0
+
+                for i in range(self.step_repeat):
+                    reward_temp = 0
+                    next_obs, reward_temp, done, truncated, info = self.env.step(action=action)
+
+                    reward += reward_temp
+
+                    if(done):
+                        break
+
+                next_obs = self.process_observation(next_obs)
+
+                self.memory.store_transition(obs, action, reward, next_obs, done)
+
+                obs = next_obs        
+
+                episode_reward += reward
+                episode_steps += 1
+                total_steps += 1
+
+                if self.memory.can_sample(batch_size):
+                    observations, actions, rewards, next_observations, dones = self.memory.sample_buffer(batch_size)
+
+                    dones = dones.unsqueeze(1).float()
+
+                    # Current Q-values from both models
+                    q_values = self.model(observations)
+                    actions = actions.unsqueeze(1).long()
+                    qsa_batch = q_values.gather(1, actions)
+
+                    # Action selection using the main models
+                    next_actions = torch.argmax(self.model(next_observations), dim=1, keepdim=True)
+
+                    # Q-value evaluation using the target models
+                    next_q_values = self.target_model(next_observations).gather(1, next_actions)
+
+                    # Compute the target using Double DQN with minimization
+                    target_b = rewards.unsqueeze(1) + (1 - dones) * self.gamma * next_q_values
+
+                    # Calculate the loss for both models
+                    loss = F.mse_loss(qsa_batch, target_b.detach())
+
+                    writer.add_scalar("Loss/model", loss.item(), total_steps)
+
+                    # Backpropagation and optimization step for both models
+                    self.model.zero_grad()
+                    loss.backward()
+                    self.optimizer_1.step()
+
+                    # Update the target models periodically
+                    if episode_steps % 4 == 0:
+                        soft_update(self.target_model, self.model)
+
+            self.model.save_the_model(filename='models/dqn1.pt')
+
+            writer.add_scalar('Score', episode_reward, episode)
+            writer.add_scalar('Epsilon', epsilon, episode)
+
+            if epsilon > min_epsilon:
+                epsilon *= epsilon_decay
+
+            episode_time = time.time() - episode_start_time
+
+            print(f"Completed episode {episode} with score {episode_reward}")
+            print(f"Episode Time: {episode_time:1f} seconds")
+            print(f"Episode Steps: {episode_steps}")
---
+++ train.py
@@ -0,0 +1,47 @@
+import random
+import time
+from agent import Agent
+import gymnasium as gym
+from gymnasium.wrappers import GrayscaleObservation
+import ale_py
+
+episodes = 10000
+max_episode_steps = 10000
+total_steps = 0
+step_repeat = 4
+max_episode_steps = max_episode_steps / step_repeat
+
+batch_size = 64
+learning_rate = 0.0001
+epsilon = 1
+min_epsilon = 0.1
+epsilon_decay = 0.995
+gamma = 0.99
+
+hidden_layer = 128
+
+# print(observation.shape)
+
+# Constants
+WINDOW_WIDTH, WINDOW_HEIGHT = 1200, 800  # Visible game window size
+WORLD_WIDTH, WORLD_HEIGHT = 1800, 1200  # The size of the larger game world
+FPS = 60
+
+env = gym.make("ALE/Pong-v5", render_mode="rgb_array")
+
+env = GrayscaleObservation(env, keep_dim=True)
+
+summary_writer_suffix = f'dqn_lr={learning_rate}_hl={hidden_layer}_mse_loss_bs={batch_size}_double_dqn'
+
+agent = Agent(env, dropout=0.2, hidden_layer=hidden_layer,
+              learning_rate=learning_rate, step_repeat=step_repeat,
+              gamma=gamma)
+
+
+# Training Phase 1
+
+agent.train(episodes=episodes, max_episode_steps=max_episode_steps, summary_writer_suffix=summary_writer_suffix + "-phase-1",
+            batch_size=batch_size, epsilon=epsilon, epsilon_decay=epsilon_decay,
+            min_epsilon=min_epsilon)
+    
+
